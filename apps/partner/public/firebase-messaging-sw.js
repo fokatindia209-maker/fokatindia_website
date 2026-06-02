70440333f5f18e4ae64e442e70b93eac9d625888
myvastuastro <myvastuastro@gmail.com>
2026-06-02 23:28:20 +0400

    fdas

---
+++ apps/partner/public/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDailC7nxUZjoRlPq1eMmNztHMbbhp9Stc",
+    authDomain: "fokatindia-21620.firebaseapp.com",
+    projectId: "fokatindia-21620",
+    messagingSenderId: "966340445316",
+    appId: "1:966340445316:web:2f3a4051de5c020940eed1"
+});
+
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log("Background message received:", payload);
+
+    self.registration.showNotification(
+        payload?.notification?.title || "Notification",
+        {
+            body: payload?.notification?.body,
+            icon: "/logo192.png",
+        }
+    );
+});
